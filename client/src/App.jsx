09df5825f401151a49bdb279768761d3962a29c5
Carlo Amaquin <amaquincj01@gmail.com>
2025-11-20 20:42:46 +0800

    feat: official tariff rate added to jurisdiction information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import RouteFinder from "./user/pages/RouteFinder";
+import RouteFinder from "./pages/RouteFinder";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
